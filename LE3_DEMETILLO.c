8cdc3261c5a3dc6b17384756a17fdfe03f4a74ad
Chara Marie Demetillo <demetillo.chara123@gmail.com>
2025-09-17 07:49:34 +0800

    Add files via upload

---
+++ LE3_DEMETILLO.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char yourCompleteName[30] = "";
+    char yourStudentSection[30] = "";
+    int myFirstNumber;
+    int mySecondNumber;
+
+    printf("Enter Complete Name: ");
+    fgets(yourCompleteName, sizeof(yourCompleteName), stdin);
+    printf("Enter Section: ");
+    fgets(yourStudentSection, sizeof(yourStudentSection), stdin);
+
+    printf("Enter First Number = ");
+    scanf("%d", &myFirstNumber);
+
+    printf("Enter Second Number = ");
+    scanf("%d", &mySecondNumber);
+
+    printf("\n");
+
+    printf("Student Name: %s", yourCompleteName);
+    printf("Section: %s", yourStudentSection);
+
+    printf(" Results: ");
+    printf("%d + %d = %d\n", myFirstNumber, mySecondNumber, myFirstNumber + mySecondNumber);
+    printf("%d - %d = %d\n", myFirstNumber, mySecondNumber, myFirstNumber - mySecondNumber);
+    printf("%d * %d = %d\n", myFirstNumber, mySecondNumber, myFirstNumber * mySecondNumber);
+    printf("%d / %d = %d\n", myFirstNumber, mySecondNumber, myFirstNumber / mySecondNumber);
+    return 0;
+}
